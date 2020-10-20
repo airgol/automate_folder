82833a35b40c5b4a6909d29f4bb76ee50d5850e0
Airam <airam@example.com>
2020-10-20 18:49:22 +0200

    First commit

--- automation.py
+++ automation.py
@@ -1,7 +1,7 @@
 import time
 import os
 # Moving to Download directory
-directory_download = "/home/cloud/Downloads" 
+directory_download = "/home/cloud/Downloads"
 os.chdir(directory_download)
 
 files = os.listdir(os.getcwd())
@@ -11,7 +11,7 @@ time_now = time.time()
 # 30 days calculated in seconds
 days = 30*24*60*60
 
-# We check every item in list; if it is a directory we do nothing 
+# We check every item in list; if it is a directory we do nothing
 # If it is a file, we check if we used it in the last 30 days,
 # We delete the files that we did not use in 30 days
 for file_name in files:
@@ -23,12 +23,12 @@ for file_name in files:
             print(file_name + "was remove")
 
 # Organizing files in Downloads Directory
-list = ['pdf', 'zip', 'gz','docx','xlsx','xls','csv','db','deb','png','jpg','jpeg']
+list = ['pdf', 'zip', 'gz', 'docx', 'xlsx', 'xls', 'csv', 'db', 'deb', 'png', 'jpg', 'jpeg']
 for file_name in files:
     if not os.path.isdir(file_name):
         for lista in list:
             # Lista folder will have he name of the extension
-            lista_f = lista 
+            lista_f = lista
             # If we want to specify the folder name for specific files extension
             # we rename them here in groups
             if lista == 'xlsx' or lista == 'xls':
@@ -37,12 +37,13 @@ for file_name in files:
                 lista_f = "gz"
             if lista == 'png' or lista == 'jpg' or lista == 'jpeg':
                 lista_f = "images"
-            
+
             if file_name.endswith("." + lista):
                 # Checking if the pdf directory exist
                 if not os.path.isdir(lista_f):
                     os.system('mkdir ' + lista_f)
                 os.system('mv "'+ file_name + '" ./' + lista_f + '/')
+
 
 
 
